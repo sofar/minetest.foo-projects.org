0977b9d38e1ecb7145b0faf4658b9ad3a89bb476
Auke Kok <sofar@foo-projects.org>
2017-09-29 19:28:35 +0000

    Put navigation in one place. Sadly, JS is the simplest solution.

---
+++ nav.js
@@ -0,0 +1,9 @@
+document.write('\
+<div style="top: 1em; width: 100%; background: #ccc; padding:0.5em; text-align: center; font-size: 140%;">\
+	<kbd><a href="/">Inside the Box</a></kbd>\
+	<kbd><a href="/rules.html">Server Rules</a></kbd>\
+	<kbd><a href="/gallery.html">Screenshot Gallery</a></kbd>\
+	<kbd><a href="/videos.html">Videos</a></kbd>\
+	<kbd><a href="/credits.html">Credits</a></kbd>\
+	<kbd><a href="https://forum.minetest.net/viewtopic.php?t=18406">Minetest Forum</a></kbd>\
+</div>')
