f4fc72ba875d09e9a6915323a7554e7eed42474f
Auke Kok <sofar@foo-projects.org>
2017-10-05 18:57:21 +0000

    Add series/box stuff. Update CSS to match.

--- nav.js
+++ nav.js
@@ -1,7 +1,8 @@
 document.write('\
-<div style="top: 1em; width: 100%; background: #ccc; padding:0.8em; text-align: center; font-size: 115%;">\
+<div style="top: 1em; width: 100%; background: #ccc; padding:0.8em; text-align: center; font-size: 150%;">\
 	<kbd><a href="/">Inside the Box</a></kbd>\
 	<kbd><a href="/rules.html">Server Rules</a></kbd>\
+	<kbd><a href="/boxes.html">Boxes</a></kbd>\
 	<kbd><a href="/gallery.html">Screenshot Gallery</a></kbd>\
 	<kbd><a href="/videos.html">Videos</a></kbd>\
 	<kbd><a href="/credits.html">Credits</a></kbd>\
