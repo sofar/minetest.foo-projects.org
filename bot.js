95a05add192d5fecd15851365eb25d6b5c1eaf45
Auke Kok <sofar@foo-projects.org>
2017-10-12 23:41:09 +0000

    Add footer at the lower end of each page.

---
+++ bot.js
@@ -0,0 +1,5 @@
+document.write('\
+<div style="bottom: 1em; height: 600px;"></div>\
+<div style="bottom: 1em; width: 100%; background: #ccc; padding:0.8em; text-align: center; font-size: 100%;">\
+	<p>Inside the Box - (C) 2017 sofar et al.</p>\
+</div>')
