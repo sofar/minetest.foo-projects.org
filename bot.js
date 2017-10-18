4be40019b5eafe146ee8a88cdf561feb36916af5
Auke Kok <sofar@foo-projects.org>
2017-10-18 21:49:16 +0000

    Add server status page.

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 document.write('\
 <div style="bottom: 1em; height: 600px;"></div>\
 <div style="bottom: 1em; width: 100%; background: #ccc; padding:0.8em; text-align: center; font-size: 100%;">\
-	<p>Inside the Box - (C) 2017 sofar et al.</p>\
+	<p>Inside the Box - (C) 2017 sofar et al. <span style="font-size: 8px;">&nbsp;<a href="/status.html">Server Status</a></span></p>\
 </div>')
