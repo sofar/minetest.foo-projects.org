05852b817dda5c9fbe0985ddbb7da2a5eb348fb3
Auke Kok <sofar@foo-projects.org>
2017-11-05 04:28:48 +0000

    Add ranking page.

--- nav.js
+++ nav.js
@@ -2,6 +2,7 @@ document.write('\
 <div style="top: 1em; width: 100%; background: #ccc; padding:0.8em; text-align: center; font-size: 150%;">\
 	<kbd><a href="/">Inside the Box</a></kbd>\
 	<kbd><a href="/rules.html">Server Rules</a></kbd>\
+	<kbd><a href="/rankings.html">Rankings</a></kbd>\
 	<kbd><a href="/boxes.html">Boxes</a></kbd>\
 	<kbd><a href="/gallery.html">Screenshot Gallery</a></kbd>\
 	<kbd><a href="/videos.html">Videos</a></kbd>\
---
+++ top.js
@@ -0,0 +1,28 @@
+function jsonget(url, id)
+{
+	var r = new XMLHttpRequest();
+	r.open("GET", url, true);
+	r.setRequestHeader("Content-type", "application/json")
+
+	r.onreadystatechange = function()
+	{
+		if (r.readyState == 4 && r.status == 200)
+		{
+			var tbl = JSON.parse(r.responseText);
+
+			var s = "<table style=\"width: 80%;margin: 0 auto;\"><tr><th>Rank</th><th>Name</th></tr>\n";
+
+			for (i = 1; i <= 10; i++)
+			{
+				s += "<tr><td>" + i.toString() + "</td><td>" + tbl[i] + "</td></tr>\n";	
+			}
+			s += "</table>\n";
+
+			document.getElementById(id).innerHTML = s;
+		}
+	}
+	r.send();
+}
+
+jsonget("http://minetest.foo-projects.org/top_players.json", "player");
+
